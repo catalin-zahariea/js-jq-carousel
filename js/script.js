94d634302fc7d17c32c38acc13a9061feb987f6e
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-04-08 17:06:20 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -12,13 +12,31 @@
 
 var arrowRight = $(".arrow.right");
 arrowRight.click(function () {
-  var activeImg = $(".slider-images > .active");
+  var activeImg = $(".slider-images .active");
   activeImg.removeClass("active");
 
   if (activeImg.hasClass("last")) {
-    $(".slider-images > .first").addClass("active");
+    $(".slider-images .first").addClass("active");
   } else {
     activeImg.next().addClass("active");
+  }
+
+  var activeRight = $(".slider-images .right-side");
+  activeRight.removeClass("right-side");
+
+  if (activeRight.hasClass("last")) {
+    $(".slider-images .first").addClass("right-side");
+  } else {
+    activeRight.next().addClass("right-side");
+  }
+
+  var activeLeft = $(".slider-images .left-side");
+  activeLeft.removeClass("left-side");
+
+  if (activeLeft.hasClass("last")) {
+    $(".slider-images .first").addClass("left-side");
+  } else {
+    activeLeft.next().addClass("left-side");
   }
 });
 
@@ -28,12 +46,30 @@ arrowRight.click(function () {
 
 var arrowLeft = $(".arrow.left");
 arrowLeft.click(function () {
-  var activeImg = $(".slider-images > .active");
+  var activeImg = $(".slider-images .active");
   activeImg.removeClass("active");
 
   if (activeImg.hasClass("first")) {
-    $(".slider-images > .last").addClass("active");
+    $(".slider-images .last").addClass("active");
   } else {
     activeImg.prev().addClass("active");
+  }
+
+  var activeRight = $(".slider-images .right-side");
+  activeRight.removeClass("right-side");
+
+  if (activeRight.hasClass("first")) {
+    $(".slider-images .last").addClass("right-side");
+  } else {
+    activeRight.prev().addClass("right-side");
+  }
+
+  var activeLeft = $(".slider-images .left-side");
+  activeLeft.removeClass("left-side");
+
+  if (activeLeft.hasClass("first")) {
+    $(".slider-images .last").addClass("left-side");
+  } else {
+    activeLeft.prev().addClass("left-side");
   }
 });
